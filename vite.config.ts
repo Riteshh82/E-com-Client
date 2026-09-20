6c5d4e0cdcbe2e844bba78b0be062a7521920536
Riteshkumar Sushil Yadav <yadavritesh6655@gmail.com>
2026-09-20 11:51:19 +0530

    push

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rolldownOptions: {
-      output: {
-        manualChunks: {
-          "vendor-react": ["react", "react-dom", "react-router-dom"],
-          "vendor-recharts": ["recharts"],
-          "vendor-lucide": ["lucide-react"],
-        },
-      },
-    },
-  },
+
   server: {
     proxy: {
       "/api": {
